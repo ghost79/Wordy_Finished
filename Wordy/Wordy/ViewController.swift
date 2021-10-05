cc228dca31ad99349997f3a5bcdd7a7794ccc9e0
Andreas Gjaerum <andreas@burnttoast.no>
2021-10-05 14:10:01 +0200

    Added word list.
    Storing the shown words in user defaults and showing them in a table view.

--- Wordy/Wordy/ViewController.swift
+++ Wordy/Wordy/ViewController.swift
@@ -22,10 +22,27 @@ class ViewController: UIViewController {
         let delegate: WordDelegate = self
         networkModel = NetworkModel(delegate: delegate)
     }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? WordTableViewController,
+           let orignalWordyWords = UserDefaults.standard.array(forKey: "wordyWords"),
+           let orignalWordyWords = orignalWordyWords as? [String] {
+                destination.wordList = orignalWordyWords
+        }
+    }
 }
 
 extension ViewController: WordDelegate {
     func showWord(_ word: String) {
+        let userDefaults = UserDefaults.standard
+
+        if let orignalWordyWords = userDefaults.array(forKey: "wordyWords"), var orignalWordyWords = orignalWordyWords as? [String] {
+            orignalWordyWords.append(word)
+            userDefaults.set(orignalWordyWords, forKey: "wordyWords")
+        } else {
+            userDefaults.set([word], forKey: "wordyWords")
+        }
+
         DispatchQueue.main.async {
             self.headingLabel.text = word
         }
